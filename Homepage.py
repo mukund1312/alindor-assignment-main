722ac3ed7b7a7946ae3f00baf0e631220a95e559
Mukund <85414863+mukund1312@users.noreply.github.com>
2024-05-05 23:34:13 +0530

    Add files via upload

---
+++ Homepage.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Alindor's Resume Review",
+    page_icon="👋",
+)
+
+st.title("Alindor's Resume Review")
+st.markdown("""
+### Features
+- Upload a resume and job description to see if a candidate passes the fit test
+- Analyze different metrics about a candidate before hiring
+
+### Author
+- Mukund Umashankar ([Github](https://github.com/mukund1312))
+""")
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# alindor-assignment-main
-assignment
+## Alindor Corp Take Home Assignment
+
+This repository contains the source code for resume review application built using OpenAI, part of take home assignment by Alindor Corp.
+
+Author: Mukund Umashankar
+
+Setup Steps:
+
+- Install requirements using `pip install -r requirements.txt`
+- Run the application using `streamlit run Homepage.py`
